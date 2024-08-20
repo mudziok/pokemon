e22d06b042ad23f82d56b68855bd011c4bbb1d8f
mudziok <mudziok@gmail.com>
2024-08-20 13:50:20 +0200

    feat: replace inter font with ibm vga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const pixelFont = localFont({
+  src: "fonts/ibm_vga_9x16.woff",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={pixelFont.className}>{children}</body>
     </html>
   );
 }
