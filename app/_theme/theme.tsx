6deaa71027ef6b0ee0a21ffbbf6911a9d3d9a985
mudziok <mudziok@gmail.com>
2024-08-20 19:14:10 +0200

    feat: style TextField component

--- app/_theme/theme.tsx
+++ app/_theme/theme.tsx
@@ -8,6 +8,29 @@ const pixelFont = localFont({
 });
 
 export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9747FF",
+      dark: "#7135BF",
+      light: "#b96eff3F",
+    },
+    grey: {
+      100: "#2A2A2A",
+      200: "#7F7F7F",
+      300: "#E4E4E4",
+      400: "#EEEEEE",
+    },
+    error: {
+      main: "#FF4E4E",
+    },
+    background: {
+      default: "#FFFFFF",
+    },
+    text: {
+      primary: "#2A2A2A",
+      secondary: "#2A2A2A",
+    },
+  },
   typography: {
     fontFamily: pixelFont.style.fontFamily,
   },
---
+++ components/TextField/TextField.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import {
+  FormControl,
+  FormHelperText,
+  InputBase,
+  InputBaseProps,
+  InputLabel,
+  styled,
+} from "@mui/material";
+import { forwardRef } from "react";
+
+export const TextFieldInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(2),
+  },
+  "&": {
+    borderRadius: 2,
+    backgroundColor: theme.palette.background.default,
+    border: "1px solid",
+    borderColor: theme.palette.grey[400],
+    fontSize: 14,
+    width: "auto",
+    padding: "14px 10px",
+    transition: theme.transitions.create(["border-color", "box-shadow"], {
+      duration: "0.1s",
+    }),
+
+    "&::placeholder": {
+      color: theme.palette.grey[200],
+      opacity: 1,
+    },
+
+    ":hover": {
+      borderColor: theme.palette.primary.main,
+    },
+
+    ":focus-within": {
+      borderColor: theme.palette.primary.main,
+      boxShadow: "0px 0px 0px 4px #9747FF40",
+    },
+  },
+}));
+
+export type TextFieldProps = InputBaseProps & {
+  label?: string;
+  helperText?: string;
+  errorText?: string;
+};
+
+export const TextField = forwardRef(function TextField(
+  props: TextFieldProps,
+  ref
+) {
+  const { label, helperText, errorText, ...rest } = props;
+  const isFormHelperTextVisible = Boolean(helperText || errorText);
+
+  return (
+    <FormControl variant="standard" fullWidth={props.fullWidth}>
+      {label && (
+        <InputLabel shrink htmlFor={props.id}>
+          {label}
+        </InputLabel>
+      )}
+      <TextFieldInput ref={ref} {...rest} />
+      {isFormHelperTextVisible && (
+        <FormHelperText error={!!errorText}>
+          {errorText ?? helperText}
+        </FormHelperText>
+      )}
+    </FormControl>
+  );
+});
