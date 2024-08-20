60194dcff7391142609a46dd28bbe627e06f7379
mudziok <mudziok@gmail.com>
2024-08-20 21:06:02 +0200

    feat: implement fuzzy search

--- app/api/pokemon/route.ts
+++ app/api/pokemon/route.ts
@@ -1,21 +1,28 @@
-import { NextRequest, NextResponse } from "next/server";
 import { promises as fs } from "fs";
-import { pokemonSchema } from "@/api/pokemon/schema";
+import { Pokemon, pokemonSchema } from "@/api/pokemon/schema";
 import { z } from "zod";
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+import Fuse from "fuse.js";
+import { NextRequest, NextResponse } from "next/server";
 
 async function readJSON(path: string) {
   const file = await fs.readFile(path, "utf8");
   return JSON.parse(file);
 }
 
-export async function GET() {
-  await sleep(500);
+export async function GET(request: NextRequest) {
   const data = await readJSON("./pokemon.json");
   const { data: pokemons } = z
     .object({ data: z.array(pokemonSchema) })
     .parse(data);
 
-  return NextResponse.json(pokemons, { status: 200 });
+  const searchedName = request.nextUrl.searchParams.get("name");
+
+  if (!searchedName) {
+    return NextResponse.json(pokemons, { status: 200 });
+  }
+
+  const fuse = new Fuse(pokemons, { keys: ["name"] });
+  const results: Pokemon[] = fuse.search(searchedName).map(({ item }) => item);
+
+  return NextResponse.json(results, { status: 200 });
 }
